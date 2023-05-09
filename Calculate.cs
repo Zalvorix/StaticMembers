5a60fa3d5eb0593c392a03decb8079559ba6f884
Zalvorix <114089240+Zalvorix@users.noreply.github.com>
2023-05-09 09:40:25 -0500

    Initial commit

---
+++ Calculate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Members_Babcock_Olivia
+{
+    static class Calculate
+    {
+        public static float result = 0.0f;
+
+        static Calculate()
+        {
+            result = 0.0f;
+        }
+
+        // Defines integer return equation
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        // Defines float return equation
+
+        public static float Add(float x, float y)
+        {
+            return x + y;
+        }
+
+        public static float Sub(float x, float y)
+        {
+            return x - y;
+        }
+
+        public static float Mult(float x, float y)
+        {
+            return x * y;
+        }
+
+        public static float Div(float x, float y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Static_Members_Babcock_Olivia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prints to the console
+            Console.WriteLine("Integer Methods");
+            // Adds, subtracts, etc x,y values and states result
+            Console.WriteLine(Calculate.Add(100, 21));
+            Console.WriteLine(Calculate.Sub(40, 9));
+            Console.WriteLine(Calculate.Mult(10, 6));
+            Console.WriteLine(Calculate.Div(20, 4));
+
+            //Prints to the console
+            Console.WriteLine("\nFloat Methods");
+            // Adds, subtracts, etc x,y values and states result
+            Console.WriteLine(Calculate.Add(100.44f, 21.11f));
+            Console.WriteLine(Calculate.Sub(40.82f, 9.99f));
+            Console.WriteLine(Calculate.Mult(10.05f, 6.73f));
+            Console.WriteLine(Calculate.Div(20.67f, 4.36f));
+        }
+    }
+}
